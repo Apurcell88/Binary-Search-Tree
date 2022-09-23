6fc7702e373d34cc66347c1e4f1eaf0eb5634c73
Adam Purcell <apurcell88@gmail.com>
2022-09-23 13:53:26 -0500

    Initial commit.

---
+++ bst.js
@@ -0,0 +1,17 @@
+// binary search tree
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const tree = new BST();
