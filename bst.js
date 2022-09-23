b02a61d2cfd6f81d363b4b5c6e80cdef7a58df22
Adam Purcell <apurcell88@gmail.com>
2022-09-23 14:19:32 -0500

    Added insert method to BST class

--- bst.js
+++ bst.js
@@ -12,6 +12,41 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
 const tree = new BST();
+tree.insert(10);
+tree.insert(14);
+tree.insert(8);
+tree.insert(15);
+tree.insert(11);
+
+console.log(tree);
